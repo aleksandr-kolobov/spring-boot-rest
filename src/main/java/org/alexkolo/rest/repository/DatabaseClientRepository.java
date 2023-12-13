dafd18ea624b09734b43613b3d83f7a59105de15
aleksandr-kolobov <forward2010@inbox.ru>
2023-12-14 02:34:00 +0700

    Refactoring for two versions

--- src/main/java/org/alexkolo/rest/repository/DatabaseClientRepository.java
+++
@@ -1,9 +0,0 @@
-package org.alexkolo.rest.repository;
-
-import org.alexkolo.rest.model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DatabaseClientRepository extends JpaRepository<Client, Long> {
-
-
-}
--- src/main/java/org/alexkolo/rest/repository/DatabaseOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package org.alexkolo.rest.repository;
-
-import org.alexkolo.rest.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DatabaseOrderRepository extends JpaRepository<Order, Long> {
-
-
-}
